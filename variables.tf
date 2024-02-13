aada1d0416ae9f1a1f039e5b90d24b397956bc55
Ravi <72593808+ravitejam-python@users.noreply.github.com>
2024-02-13 14:19:56 -0500

    Created variables.tf

---
+++ variables.tf
@@ -0,0 +1,30 @@
+
+
+variable "tag_name" { 
+    description = "Tag Name of for Ec2 instance" 
+    default     = "my-ec2-instance" 
+} 
+
+variable "versioning" {
+    type        = bool
+    description = "(Optional) A state of versioning."
+    default     = true
+}
+variable "acl" {
+    type        = string
+    description = " Defaults to private "
+    default     = "private"
+}
+variable "bucket_prefix" {
+    type        = string
+    description = "(required since we are not using 'bucket') Creates a unique bucket name beginning with the specified prefix"
+    default     = "my-s3bucket-"
+}
+variable "tags" {
+    type        = map
+    description = "(Optional) A mapping of tags to assign to the bucket."
+    default     = {
+        environment = "DEV"
+        terraform   = "true"
+    }
+}
